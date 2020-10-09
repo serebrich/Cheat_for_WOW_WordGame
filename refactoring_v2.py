dec069958cae18e3007ba9637ed7459fc04f6505
Dmitryi Serebrich <59286880+serebrich@users.noreply.github.com>
2020-10-09 18:56:02 +0300

    Update refactoring_v2.py

--- refactoring_v2.py
+++ refactoring_v2.py
@@ -1,22 +1,18 @@
 import itertools
 import functools
-from datetime import datetime
 
-words = input('Input all words in game\n')
-start_time = datetime.now()
 
-final_words = []
+def letters2words(letters: str) -> set:
+    final_words = []
 
-def str_combination(word):
-    for n in range(3, len(word) + 1):
-        yield from map(functools.partial(str.join, ''), itertools.permutations(word, n))
+    def str_combination(word):
+        for n in range(3, len(word) + 1):
+            yield from map(
+                functools.partial(str.join, ""), itertools.permutations(word, n)
+            )
 
-with open('Slovar.txt', 'r', encoding='utf-8') as file:
-    data = file.read()
-    data = set(data.split())
-    final_words = data.intersection(str_combination(words))
+    with open("Slovar.txt", "r", encoding="utf-8") as file:
+        data = set(file.read().split())
+        final_words = data.intersection(str_combination(letters))
 
-for position, word in enumerate(set(final_words), start=1):
-    print(f'{position}. {word}')
-
-print(datetime.now() - start_time)
+    return final_words
